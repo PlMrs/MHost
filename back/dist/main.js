7d63c2b32f814303b1ab0635436b537c103035f8
Marais Paul <paulmaraispro@gmail.com>
2022-07-04 11:00:36 +0200

    ch

--- back/dist/main.js
+++ back/dist/main.js
@@ -21,7 +21,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('doc', app, document);
-    await app.listen(3000);
+    await app.listen(80);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
